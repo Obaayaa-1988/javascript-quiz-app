c65fa7558df111911cbfab83e716067d1a172e2b
Obaayaa-1988 <aquaicoe31@gmail.com>
2022-03-21 09:38:18 +0000

    quiz-app almost completed

---
+++ quiz.js
@@ -0,0 +1,53 @@
+ const quizData = [
+     {
+       question: 'How old is Abigail?',
+       a: '10',
+       b: '23',
+       c: '32',
+       d: '98',
+       correct: 'c'
+     }, {
+        question: 'How old is Abigail older Sister?',
+        a: '10',
+        b: '23',
+        c: '98',
+        d: '43',
+        correct: 'd'
+
+     }, {
+        question: 'How old is Abigails Mother?',
+        a: '64',
+        b: '23',
+        c: '98',
+        d: '32',
+        correct: 'a'
+
+     }, {
+        question: 'How old is Abigails younger brother ?',
+        a: '10',
+        b: '30',
+        c: '98',
+        d: '32',
+        correct: 'b'
+            
+     }, {
+        question: 'How old is Abigails boyfriend?',
+        a: '10',
+        b: '23',
+        c: '47',
+        d: '32',
+        correct: 'c'
+            
+     } , {
+        question: 'How old is Abigails bestfriend?',
+        a: '10',
+        b: '23',
+        c: '98',
+        d: '31',
+        correct: 'd'
+            
+     } 
+
+
+
+ ]
